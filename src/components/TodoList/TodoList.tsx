d9b7d61b7ca5bd96b66be7cb80f2bf96062968ed
Artem Nosachenko <nosachenko.art@icloud.com>
2026-04-28 18:20:46 +0200

    add correction (2)

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,1 +1,11 @@
-export const TodoList = () => {};
+import { TodoInfo } from '../TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+      ))}
+    </section>
+  );
+};
